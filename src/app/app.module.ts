4d6de43d91fb2e68d18a17bc83a0f10613b48765
riad <ameririyadyousef@gmail.com>
2024-06-30 12:42:53 +0100

    creating Angular aplication , adding Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
+import { AppFooterComponent } from './layout/app-footer/app-footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    HomeComponent,
+    ServiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
