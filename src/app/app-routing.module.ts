1c22e8d0ca2c938c49c8d6fd2b6d9f752f699058
riad <ameririyadyousef@gmail.com>
2024-07-01 21:05:17 +0100

    appFooter . homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path : '', component : HomeComponent}
+  {path : '', component : HomeComponent},
+  {path: 'home' , component : HomeComponent},
+  {path : '**' , component : HomeComponent}
 ];
 
 @NgModule({
